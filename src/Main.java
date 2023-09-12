48736c22a21e0784000d350008bbab0221e507d0
Your Name <you@example.com>
2023-09-12 08:13:07 +0530

    first attempt

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+
+    public static void main(String[]args) {
+        RWOnly obj = new RWOnly();
+        obj.name = "ABhishek";
+        System.out.println(obj.name);
+        }
---
+++ src/RWOnly.java
@@ -0,0 +1,9 @@
+public class RWOnly{
+    private String name;
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String s){
+        this.name = s;
+    }
+}
